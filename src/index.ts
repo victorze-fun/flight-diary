fd73f7f1cfd2b42169411205d87660c9bda96867
Victor Zevallos <victorze@protonmail.com>
2021-11-14 15:49:28 -0500

    Change project structure

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 const app = express();
+import diaryRouter from './routes/diaries';
 
 app.use(express.json());
 
@@ -7,6 +8,8 @@ app.get('/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.use('/api/diaries', diaryRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => {
