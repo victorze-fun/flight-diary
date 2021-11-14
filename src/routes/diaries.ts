b4ba2b62458145bc6b5caa90558e20b05713d010
Victor Zevallos <victorze@protonmail.com>
2021-11-14 18:04:51 -0500

    Add validation

--- src/routes/diaries.ts
+++ src/routes/diaries.ts
@@ -1,6 +1,6 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import express from 'express';
 import diaryService from '../services/diaryService';
+import toNewDiaryEntry from '../utils';
 
 const router = express.Router();
 
@@ -19,14 +19,19 @@ router.get('/', (_req, res) => {
 });
 
 router.post('/', (req, res) => {
-  const { date, weather, visibility, comment } = req.body;
-  const newDiaryEntry = diaryService.addDiary({
-    date,
-    weather,
-    visibility,
-    comment,
-  });
-  res.json(newDiaryEntry);
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    const newDiaryEntry = toNewDiaryEntry(req.body);
+
+    const addedEntry = diaryService.addDiary(newDiaryEntry);
+    res.json(addedEntry);
+  } catch (error: unknown) {
+    let errorMessage = 'Something went wrong.';
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    res.status(400).send(errorMessage);
+  }
 });
 
 export default router;
